af004b9ce09b3f24e1f0efef7f4d80526e3d66eb
Sebastián C <calderonsebastian211@gmail.com>
2023-11-28 08:51:21 -0500

    Subida 0.2

--- prototipo01.py
+++ prototipo01.py
@@ -1,69 +1,83 @@
 
 # Por ahora, como prototipo, sólo se trabajará con la tabla Categories  
 # El código se podría extender en el futuro para incluir más tablas
+# C:\Users\calde\OneDrive\Documentos\Universidad\Base de Datos\crud
 
 import sqlite3
 
 def menu():
-    print("1. Listar categorías (mostrar todas las categorías)")
-    print("2. Agregar categoría (ingresar una nueva categoría)")  
-    print("3. Modificar categoría (editar una categoría existente)")
-    print("4. Eliminar categoría (borrar una categoría)")
-    print("5. Salir (cerrar el programa)")
+    print("==========================================================")
+    print("  1. Listar categorías (mostrar todas las categorías)")
+    print("  2. Agregar categoría (ingresar una nueva categoría)")  
+    print("  3. Modificar categoría (editar una categoría existente)")
+    print("  4. Eliminar categoría (borrar una categoría)")
+    print("  5. Salir (cerrar el programa)")
+    print("===========================================================")
 
 def listar_categorias():
-    conn = sqlite3.connect('northwind.db')
+    conn = sqlite3.connect('Northwind.db')
     c = conn.cursor()
     try:
         c.execute("SELECT CategoryID, CategoryName, Description FROM Categories") 
         categorias = c.fetchall()
-        print("Listado de Categorías:")
+        print("===========================================================")
+        print("  Listado de Categorías:\n")
         for categoria in categorias:
-            print(categoria[0], categoria[1], categoria[2])
+            print(f"  |   {categoria[0]}  |   {categoria[1]}  |   {categoria[2]}")
     except sqlite3.Error as e:
-        print("Error al listar las categorías:", e)
+        print("===========================================================")
+        print("  Error al listar las categorías:", e)
     finally:
         conn.close()
 
 def agregar_categoria():
-    conn = sqlite3.connect('northwind.db')
+    conn = sqlite3.connect('Northwind.db')
     c = conn.cursor()
     try:
-        nombre = input("Ingrese el nombre de la nueva categoría: ")
-        descripcion = input("Ingrese una descripción: ")
+        print("===========================================================")
+        nombre = input("  Ingrese el nombre de la nueva categoría: ")
+        descripcion = input("  Ingrese una descripción: ")
         c.execute("INSERT INTO Categories (CategoryName, Description) VALUES (?, ?)", (nombre, descripcion))
         conn.commit()
-        print("Categoría agregada exitosamente!")
+        print("===========================================================")
+        print("  Categoría agregada exitosamente!")
     except sqlite3.Error as e:
-        print("Error al agregar la categoría:", e)
+        print("===========================================================")
+        print("  Error al agregar la categoría:", e)
     finally:
         conn.close()
 
 def modificar_categoria():
-    conn = sqlite3.connect('northwind.db')
+    conn = sqlite3.connect('Northwind.db')
     c = conn.cursor()
     try:
-        id = int(input("Ingrese el ID de la categoría a modificar: "))
-        nuevo_nombre = input("Ingrese el nuevo nombre: ")
-        nueva_descripcion = input("Ingrese la nueva descripción: ")
+        print("===========================================================")
+        id = int(input("  Ingrese el ID de la categoría a modificar: "))
+        nuevo_nombre = input("  Ingrese el nuevo nombre: ")
+        nueva_descripcion = input("  Ingrese la nueva descripción: ")
         c.execute("UPDATE Categories SET CategoryName = ?, Description = ? WHERE CategoryID = ?", (nuevo_nombre, nueva_descripcion, id))
         conn.commit()
-        print("Categoría modificada exitosamente!")
+        print("===========================================================")
+        print("  Categoría modificada exitosamente!")
     except sqlite3.Error as e:
-        print("Error al modificar la categoría:", e)
+        print("===========================================================")
+        print("  Error al modificar la categoría:", e)
     finally:
         conn.close()  
 
 def eliminar_categoria():
-    conn = sqlite3.connect('northwind.db')
+    conn = sqlite3.connect('Northwind.db')
     c = conn.cursor()
     try:
-        id = int(input("Ingrese el ID de la categoría a eliminar: "))
+        print("===========================================================")
+        id = int(input("  Ingrese el ID de la categoría a eliminar: "))
         c.execute("DELETE FROM Categories WHERE CategoryID = ?", (id,))
         conn.commit()
-        print("Categoría eliminada exitosamente!")
+        print("===========================================================")
+        print("  Categoría eliminada exitosamente!")
     except sqlite3.Error as e:
-        print("Error al eliminar la categoría:", e)
+        print("===========================================================")
+        print("  Error al eliminar la categoría:", e)
     finally:
         conn.close()
 
@@ -71,7 +85,7 @@ opcion = 0
 while opcion != 5:
     menu()
     try:
-        opcion = int(input("Ingrese una opción: "))
+        opcion = int(input("  Ingrese una opción: "))
         if opcion == 1:
             listar_categorias()
         elif opcion == 2:
@@ -81,8 +95,10 @@ while opcion != 5:
         elif opcion == 4:
             eliminar_categoria()
         elif opcion == 5:
-            print("Saliendo...") 
+            print("===========================================================")
+            print("  Saliendo...")
+            print("===========================================================") 
         else:
-            print("Opción inválida")
+            print("  Opción inválida")
     except ValueError:
-        print("Por favor, ingrese un número válido.")
+        print("  Por favor, ingrese un número válido.")
